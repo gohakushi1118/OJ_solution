8dc1285aac0007540b390904614cde9aba2d9a71
gohakushi1118 <gohakushi@gmail.com>
2026-06-05 21:20:56 +0800

    2026/06/05

---
+++ AtCoder/abc454_A.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int l, r;
+    cin >> l >> r;
+    cout << r - l + 1 << "\n";
+    return 0;
+}
---
+++ AtCoder/abc455_A.py
@@ -0,0 +1,6 @@
+num = list(input().split(" "))
+
+if num[0] != num[1] and num[1] == num[2]:
+    print("Yes")
+else:
+    print("No")
