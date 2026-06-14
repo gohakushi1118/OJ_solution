8dfca4fd3cf0468a77e06d0eb115ffabea7ec61e
gohakushi1118 <gohakushi@gmail.com>
2026-06-14 22:55:35 +0800

    2026/06/14

---
+++ AtCoder/abc461_A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, d;
+    cin >> a >> d;
+
+    if (a > d){
+        cout << "No" << "\n";
+    }
+    else{
+        cout << "Yes" << "\n";
+    }   
+}
